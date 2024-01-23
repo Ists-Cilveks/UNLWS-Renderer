38591fe611f7f954bc580aae46049c0204e2b475
Ists-Cilveks <istscilveks@gmail.com>
2024-01-23 23:30:39 +0200

    Add UNLWS_Text test

--- UNLWS_editor.gd
+++ UNLWS_editor.gd
@@ -1,9 +1,16 @@
 extends Control
 
+var glyphs = Glyph_List.glyphs
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	var test_text = UNLWS_Text.new()
+	test_text.add_glyph(Glyph_Instance.new(glyphs["cat"]))
+	test_text.add_glyph(Glyph_Instance.new(glyphs["eat"], Vector2(5, 0)))
+	var xml_string = test_text.xml_node.get_string()
+	var test_file = FileAccess.open("res://Images/Output/text.svg", FileAccess.WRITE)
+	test_file.store_string(xml_string)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
