dc5fd674c5466b21f56f98c353b27356646a3173
Ists-Cilveks <istscilveks@gmail.com>
2024-02-03 16:37:28 +0200

    Prevent keyboard pressing GUI buttons if there's text in the search

--- Scripts/UI/GlyphSearch.gd
+++ Scripts/UI/GlyphSearch.gd
@@ -23,43 +23,25 @@ func cancel_input():
 
 func input_complete():
 	var glyph_name = $GlyphSearchEntry.text
+	var key_handled = false
+	if glyph_name:
+		key_handled = true
 	if glyph_name in glyphs:
 		Undo_Redo.create_action("Overwrite held glyph with search")
 		var glyph_type = glyphs[glyph_name]
 		var instance = glyph_instance_scene.instantiate()
 		instance.init(glyph_type)
 		Event_Bus.glyph_search_succeeded.emit(instance)
-		#var emitter = func(): Event_Bus.glyph_search_succeeded.emit(instance)
-		#var create_and_emit_current_instance = func create_and_emit_current_instance():
-			##return emitter
-			#Event_Bus.glyph_search_succeeded.emit(instance)
-		#Undo_Redo.add_do_method(create_and_emit_current_instance)
-		#Undo_Redo.undo_redo.add_do_method(create_and_emit_current_instance)
-		#Undo_Redo.undo_redo.add_do_method(func create_and_emit_current_instance():
-			##var glyph_type = glyphs[glyph_name]
-			##var instance = glyph_instance_scene.instantiate()
-			##instance.init(glyph_type)
-			##Event_Bus.glyph_search_succeeded.emit(instance)
-		#)
-		#Undo_Redo.undo_redo.add_do_method(func create_and_emit_current_instance(): Event_Bus.glyph_search_succeeded.emit(glyph_instance_scene.instantiate()))
-		#Undo_Redo.undo_redo.add_do_method(func create_and_emit_current_instance(): test_instance.init(glyphs[glyph_name]))
-		#var create_and_emit_current_instance = func create_and_emit_current_instance():
-		#var glyph_type = glyphs[glyph_name]
-		#var instance = glyph_instance_scene.instantiate()
-		#instance.init(glyph_type)
-		#instance 
-		#Undo_Redo.add_do_method(func create_and_emit_current_instance():
-			#return emitter
-			#Event_Bus.glyph_search_succeeded.emit(instance))
-		#Undo_Redo.add_do_method(func(): Event_Bus.glyph_search_succeeded.emit(instance))
 		Undo_Redo.commit_action()
 	cancel_input()
+	return key_handled
 
 func _input(event):
 #	var glyph_input_box = $"GlyphSearch/SearchEntry"
 #	var glyph_input_box = self
 	if event is InputEventKey and event.pressed:
 #		print(event.as_text())
+		var key_handled = true
 		if event.is_action_pressed("ui_text_backspace_all_to_left"):
 			erase_all_text()
 		elif event.is_action_pressed("ui_cancel"):
@@ -68,7 +50,7 @@ func _input(event):
 			backspace_input()
 		elif event.is_action_pressed("ui_accept"):
 #			$"Mouse/HeldGlyph".change_glyph(self, $GlyphSearchEntry.text)
-			input_complete()
+			key_handled = input_complete()
 		else:
 #			var key = OS.get_keycode_string(event.keycode).to_lower()
 #			var key = OS.get_keycode_string(event.unicode).to_lower()
@@ -79,3 +61,8 @@ func _input(event):
 	#			$"GlyphSearch/SearchEntry".grab_focus()
 	#			print(event.as_text())
 				append_text(key)
+			else:
+				key_handled = false
+		if key_handled:
+			# Prevents buttons from being pressed by enter or space if the gui buttons are focused
+			accept_event()
