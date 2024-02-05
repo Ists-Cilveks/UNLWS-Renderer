bf67b990c1a5a540e1e13df49943502798647cab
Ists-Cilveks <istscilveks@gmail.com>
2024-02-05 20:04:27 +0200

    Move mouse hiding from the editor to the GUI The GUI is responsible for overriding the mouse style and is the reason for overriding it (otherwise glyphs lag behind the cursor)

--- Scripts/UI/ui_layer.gd
+++ Scripts/UI/ui_layer.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.is_action_pressed("ui_redo") or event.is_action_pressed("shift_ui_undo"):
--- Scripts/UNLWS_editor.gd
+++ Scripts/UNLWS_editor.gd
@@ -4,7 +4,6 @@ var glyphs = Glyph_List.glyphs
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	# TODO: this isn't a great place to create the necessary empty folders.
 	var res_dir = DirAccess.open("res://")
 	res_dir.make_dir("Output")
