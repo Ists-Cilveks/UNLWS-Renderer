29c087d0ab30286bfe94df6ed328eeccb008a05b
Ists-Cilveks <istscilveks@gmail.com>
2024-02-13 12:01:22 +0200

    Use shortcut keys to handle undo/redo and add an edit glyph button

--- Scripts/UI/ui_layer.gd
+++ Scripts/UI/ui_layer.gd
@@ -3,9 +3,9 @@ extends CanvasLayer
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.is_action_pressed("ui_redo") or event.is_action_pressed("shift_ui_undo"):
-			Undo_Redo.redo()
-		elif event.is_action_pressed("ui_undo"):
-			Undo_Redo.undo()
+#func _input(event):
+	#if event is InputEventKey and event.pressed:
+		#if event.is_action_pressed("ui_redo") or event.is_action_pressed("shift_ui_undo"):
+			#Undo_Redo.redo()
+		#elif event.is_action_pressed("ui_undo"):
+			#Undo_Redo.undo()
